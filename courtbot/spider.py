f45daaee5327f61555961a4c8c80729767bf6e56
Renzo Lucioni <renzo@lucioni.xyz>
2017-07-03 16:51:30 -0400

    Always quit Chrome
    Saves memory by quitting Chrome when we're done with it, even when there's a problem booking a court.

--- courtbot/spider.py
+++ courtbot/spider.py
@@ -29,6 +29,8 @@ class Spider:
         self.cache = TTLCache(maxsize, settings.CACHE_TTL)
 
     def start_chrome(self):
+        logger.info('Starting Chrome.')
+
         options = webdriver.ChromeOptions()
         options.binary_location = '/usr/bin/google-chrome-stable'
         options.add_argument('headless')
@@ -40,7 +42,11 @@ class Spider:
         self.driver.implicitly_wait(10)
 
     def quit_chrome(self):
-        # Shuts down the ChromeDriver executable.
+        """
+        Shuts down the ChromeDriver executable.
+        """
+        logger.info('Quitting Chrome.')
+
         self.driver.quit()
 
     @cachedmethod(operator.attrgetter('cache'))
@@ -149,49 +155,50 @@ class Spider:
 
         self.start_chrome()
 
-        cookies = self.login()
-        logger.info(f'Cookies present: {cookies}')
+        try:
+            cookies = self.login()
+            logger.info(f'Cookies present: {cookies}')
 
-        schedule_data = {
-            'ScheduleDate': date_string,
-            'Duration': 60,
-            'Resource': f'Zesiger Squash Court #{number}',
-            'Provider': '',
-            'SiteId': 1261,
-            'ProviderId': 0,
-            # This resource ID must be a string. Don't ask me why.
-            'ResourceId': str(constants.COURTS[number]),
-            'ServiceId': 4,
-            'ServiceName': 'Recreational Squash',
-            'ServiceUniqueIdentifier': '757170ab-4338-4ff6-868d-2fb51cc449f8',
-        }
+            schedule_data = {
+                'ScheduleDate': date_string,
+                'Duration': 60,
+                'Resource': f'Zesiger Squash Court #{number}',
+                'Provider': '',
+                'SiteId': 1261,
+                'ProviderId': 0,
+                # This resource ID must be a string. Don't ask me why.
+                'ResourceId': str(constants.COURTS[number]),
+                'ServiceId': 4,
+                'ServiceName': 'Recreational Squash',
+                'ServiceUniqueIdentifier': '757170ab-4338-4ff6-868d-2fb51cc449f8',
+            }
 
-        data = {
-            # The values for both of these keys need to be strings. (╯ಠ_ಠ）╯︵ ┻━┻
-            'scheduleInformation': json.dumps(schedule_data, separators=(',', ':')),
-            'startTime': str(hour * 60),
-        }
+            data = {
+                # The values for both of these keys need to be strings. (╯ಠ_ಠ）╯︵ ┻━┻
+                'scheduleInformation': json.dumps(schedule_data, separators=(',', ':')),
+                'startTime': str(hour * 60),
+            }
 
-        # Specify separators with no trailing whitespace for compact encoding.
-        data = json.dumps(data, separators=(',', ':'))
+            # Specify separators with no trailing whitespace for compact encoding.
+            data = json.dumps(data, separators=(',', ':'))
 
-        # This request "stages" a reservation, but doesn't complete it. Clicking
-        # through the booking process from this point is easier than dealing with
-        # the crazy ASP.NET form submit required to complete the booking.
-        url = self.base_url + '/MIT/Library/OlsService.asmx/SetScheduleInformation'
-        response = requests.post(url, headers=settings.REQUEST_HEADERS, cookies=cookies, data=data)
+            # This request "stages" a reservation, but doesn't complete it. Clicking
+            # through the booking process from this point is easier than dealing with
+            # the crazy ASP.NET form submit required to complete the booking.
+            url = self.base_url + '/MIT/Library/OlsService.asmx/SetScheduleInformation'
+            response = requests.post(url, headers=settings.REQUEST_HEADERS, cookies=cookies, data=data)
 
-        if response.status_code != 200:
-            raise BookingError
+            if response.status_code != 200:
+                raise BookingError
 
-        confirm_path = '/MIT/Members/Scheduler/AddFamilyMembersScheduler.aspx'
-        self.driver.get(self.base_url + confirm_path)
+            confirm_path = '/MIT/Members/Scheduler/AddFamilyMembersScheduler.aspx'
+            self.driver.get(self.base_url + confirm_path)
 
-        confirm_button = self.driver.find_element_by_css_selector('#ctl00_pageContentHolder_btnContinueCart')
-        confirm_button.click()
+            confirm_button = self.driver.find_element_by_css_selector('#ctl00_pageContentHolder_btnContinueCart')
+            confirm_button.click()
 
-        # Verify that booking succeeded by waiting for a unique element to appear
-        # on the booking confirmation/receipt page.
-        self.driver.find_element_by_css_selector('#ctl00_pageContentHolder_lblThankYou')
-
-        self.quit_chrome()
+            # Verify that booking succeeded by waiting for a unique element to appear
+            # on the booking confirmation/receipt page.
+            self.driver.find_element_by_css_selector('#ctl00_pageContentHolder_lblThankYou')
+        finally:
+            self.quit_chrome()
